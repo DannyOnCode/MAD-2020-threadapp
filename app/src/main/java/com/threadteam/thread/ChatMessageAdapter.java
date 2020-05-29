2b7dea2ea1b8a90dfc74fb4f07d5f23b27516756
Yee Jun Eugene Long <Orbitalsqwib@gmail.com>
2020-05-30 07:34:42 +0800

    created adapter and viewholder for chat message items. also started some scaffolding for the chat activity, and added recyclertouchlistener. minor xml tweaks.

---
+++ app/src/main/java/com/threadteam/thread/ChatMessageAdapter.java
@@ -0,0 +1,58 @@
+package com.threadteam.thread;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.threadteam.thread.models.ChatMessage;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class ChatMessageAdapter extends RecyclerView.Adapter<ChatMessageViewHolder> {
+
+    //DATA STORE
+    List<ChatMessage> chatMessageList;
+
+    public ChatMessageAdapter(List<ChatMessage> chatMessages) {
+        this.chatMessageList = chatMessages;
+    }
+
+    @NonNull
+    @Override
+    public ChatMessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        ChatMessageViewHolder viewHolder;
+
+        View item = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.activity_partial_chatmessage,
+                parent,
+                false
+        );
+
+        viewHolder = new ChatMessageViewHolder(item);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChatMessageViewHolder holder, int position) {
+        holder.MessageTextView.setText(chatMessageList.get(position).get_message());
+        holder.SenderTextView.setText(chatMessageList.get(position).get_sender());
+
+        Timestamp ts = chatMessageList.get(position).get_timestamp();
+        Date date = new Date(ts.getTime());
+        String formattedString = new SimpleDateFormat("d/MM/yyyy h:mm:a").format(date);
+
+        holder.TimestampTextView.setText(formattedString);
+    }
+
+    @Override
+    public int getItemCount() {
+        return chatMessageList.size();
+    }
+}
--- app/src/main/java/com/threadteam/thread/activities/ChatActivity.java
+++ app/src/main/java/com/threadteam/thread/activities/ChatActivity.java
@@ -1,16 +1,88 @@
 package com.threadteam.thread.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.threadteam.thread.ChatMessageAdapter;
 import com.threadteam.thread.R;
+import com.threadteam.thread.models.ChatMessage;
+
+import java.util.List;
 
 public class ChatActivity extends AppCompatActivity {
+
+    // DATA STORE
+    private List<ChatMessage> chatMessageList;
+    private ChatMessageAdapter adapter;
+
+    // VIEW OBJECTS
+    private RecyclerView ChatMessageRecyclerView;
+    private EditText MessageEditText;
+    private Button SendMsgButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        // BIND OBJECTS
+        ChatMessageRecyclerView = (RecyclerView) findViewById(R.id.chatMessageRecyclerView);
+        MessageEditText = (EditText) findViewById(R.id.messageEditText);
+        SendMsgButton = (Button) findViewById(R.id.sendMsgButton);
+
+        SendMsgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
+
+        chatMessageList = loadMessagesFromServer(25, 0);
+
+        adapter = new ChatMessageAdapter(chatMessageList);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+
+        ChatMessageRecyclerView.setLayoutManager(layoutManager);
+        ChatMessageRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        ChatMessageRecyclerView.setAdapter(adapter);
+
+        // No touch listener for now. (View only)
+
+        // Load more messages (if possible) when top is reached
+        RecyclerView.OnScrollListener scrollListener = new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                final LinearLayoutManager llm = (LinearLayoutManager) recyclerView.getLayoutManager();
+                if(llm.findLastVisibleItemPosition() == chatMessageList.size()-1) {
+                    loadMoreMessages();
+                }
+
+            }
+        };
+
+        ChatMessageRecyclerView.addOnScrollListener(scrollListener);
+    }
+
+    private void sendMessage() {
+        //TODO: Send message after send message button is clicked
+    }
+
+    private List<ChatMessage> loadMessagesFromServer(Integer numMsg, Integer startIndex) {
+        //TODO: get numMsg messages from the server starting from startIndex and return it in List<ChatMessage> format
+        return null;
+    }
+
+    private void loadMoreMessages() {
+        //TODO: Load more data implementation
     }
 }
