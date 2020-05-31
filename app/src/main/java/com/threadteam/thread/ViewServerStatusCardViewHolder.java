06f5c244912519e9cdaf601281e1962684f58cb4
Yu Tian Danny Chan <s10196363@connect.np.edu.sg>
2020-05-31 22:55:28 +0800

    added viewHolder and xml serverDivider

---
+++ app/src/main/java/com/threadteam/thread/ViewServerStatusCardViewHolder.java
@@ -0,0 +1,30 @@
+package com.threadteam.thread;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+public class ViewServerStatusCardViewHolder extends RecyclerView.ViewHolder {
+
+    // VIEW OBJECTS
+    public TextView serverName;
+    public TextView serverTitle;
+    public TextView serverLevel;
+    private CardView serverRedirectionCard;
+
+    public ViewServerStatusCardViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        serverName = (TextView) itemView.findViewById(R.id.serverNameDisplay);
+        serverTitle = (TextView) itemView.findViewById(R.id.userNameDisplay);
+        serverLevel = (TextView) itemView.findViewById(R.id.levelDisplay);
+        serverRedirectionCard = (CardView) itemView.findViewById(R.id.serverRedirect);
+    }
+
+}
