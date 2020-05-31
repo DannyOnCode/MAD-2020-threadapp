907aee2c9df36c7925cbcd8c057eac544e956d02
Yu Tian Danny Chan <s10196363@connect.np.edu.sg>
2020-06-01 01:38:03 +0800

    Installed Firebase with all dependencies Installed Picasso dependencies for image Done ViewHolders for Profile Page Updated User Object Class

--- app/src/main/java/com/threadteam/thread/models/User.java
+++ app/src/main/java/com/threadteam/thread/models/User.java
@@ -1,17 +1,70 @@
 package com.threadteam.thread.models;
 
+import android.net.Uri;
 import android.widget.ImageView;
 
 public class User {
 
-    // Template User class placeholder
 
     // Instance-specific variables
     private Integer _id;
     private String _username;
+    private String _profileImage;
+    private String _aboutUsMessage;
+    private String _statusMessage;
+    // Template User class placeholder
+    public User(){
 
-    public User(Integer id, String name) {
-        this._id = id;
-        this._username = name;
     }
+
+    public User(Integer _id, String _username, String _profileImage, String _aboutUsMessage, String _statusMessage) {
+        this._id = _id;
+        this._username = _username;
+        this._profileImage = _profileImage;
+        this._aboutUsMessage = _aboutUsMessage;
+        this._statusMessage = _statusMessage;
+    }
+    public Integer get_id() {
+        return _id;
+    }
+
+    public void set_id(Integer _id) {
+        this._id = _id;
+    }
+
+    public String get_username() {
+        return _username;
+    }
+
+    public void set_username(String _username) {
+        this._username = _username;
+    }
+
+    public String get_profileImage() {
+        return _profileImage;
+    }
+
+    public void set_profileImage(String _profileImage) {
+        this._profileImage = _profileImage;
+    }
+
+    public String get_aboutUsMessage() {
+        return _aboutUsMessage;
+    }
+
+    public void set_aboutUsMessage(String _aboutUsMessage) {
+        this._aboutUsMessage = _aboutUsMessage;
+    }
+
+    public String get_statusMessage() {
+        return _statusMessage;
+    }
+
+    public void set_statusMessage(String _statusMessage) {
+        this._statusMessage = _statusMessage;
+    }
+
+
+
+
 }
