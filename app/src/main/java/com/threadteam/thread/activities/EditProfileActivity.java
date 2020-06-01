59f98a67e256d839b9fc49663d9afc7749865809
Yee Jun Eugene Long <Orbitalsqwib@gmail.com>
2020-06-01 23:03:27 +0800

    fixed topnavtoolbar auto resizing for landscape, refactored editprofile to fit file naming convention

--- app/src/main/java/com/threadteam/thread/activities/EditProfileActivity.java
+++ app/src/main/java/com/threadteam/thread/activities/EditProfileActivity.java
@@ -11,6 +11,6 @@ public class EditProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.editprofile);
+        setContentView(R.layout.activity_editprofile);
     }
 }
