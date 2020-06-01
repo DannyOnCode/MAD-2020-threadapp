57eaec41665aa2147c1f87de2fe29c9316de3282
Yu Tian Danny Chan <s10196363@connect.np.edu.sg>
2020-06-01 17:12:59 +0800

    Added EditProfile Page as well as Activity for EditProfile

---
+++ app/src/main/java/com/threadteam/thread/activities/EditProfileActivity.java
@@ -0,0 +1,16 @@
+package com.threadteam.thread.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.threadteam.thread.R;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.editprofile);
+    }
+}
