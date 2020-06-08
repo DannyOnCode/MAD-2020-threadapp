4d7ee1d48893f6e3ea07b9737e85c7b2bb6e5cd0
s10196396@connect.np.edu.sg <s10196396@connect.np.edu.sg>
2020-06-08 08:58:13 +0800

    ADDED DOCUMENTATION AND LOGGIN TO REGISTER AND LOGIN ACTIVITY

--- app/src/main/java/com/threadteam/thread/activities/LoginActivity.java
+++ app/src/main/java/com/threadteam/thread/activities/LoginActivity.java
@@ -25,22 +25,74 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.threadteam.thread.LogHandler;
 import com.threadteam.thread.R;
 
+
+// LOGIN ACTIVITY
+//
+// PROGRAMMER-IN-CHARGE:
+// MOHAMED THABITH, S10196396B
+//
+// DESCRIPTION
+// Handles logging in of user
+// Handles resetting password of user
+//
+// NAVIGATION
+// PARENT: LOGIN ACTIVITY
+// CHILDREN: NONE
+// OTHER: VIEW SERVER
+
+
+
 public class LoginActivity extends AppCompatActivity {
+
+
+
+    // LOGGING
+    private LogHandler logHandler = new LogHandler("Login Activity");
+
+
+    // FIREBASE
+    //
+    //    // fAuth:            FIREBASE AUTH INSTANCE FOR THE CURRENT SESSION
+    private FirebaseAuth fAuth;
+
+    // VIEW OBJECTS
+    //
+    // _EmailLog:               CONTAINS EMAIL OF USER TO BE RETRIEVED FROM DATABASE
+    // _Password    :               CONTAINS PASSWORD OF USER TO BE RETRIEVED FROM DATABASE
+    // _EmailReset:             CONTAINS EMAIL OF USER TO BE RESET
+    // _LoginBtn:               TRIGGERS LOGGING IN TO APP
+    // _ResetBtn                TRIGGERS RESETTING PASSWORD
+    // _RegisterBtn:            TRIGGERS RESET CARD VIEW
+    // _ForgotPwBtn:            TRIGGERS RESET PASSWORD
+    // _CancelBtn:              TRIGGERS RETURN TO LOGIN CARD VIEW
+    // progressBar              DISPLAYS LOG IN PROGRESS ONCE _LoginBtn HAS BEEN CLICKED
+    // _LoginCard               DISPLAY LOGIN FIELDS AND BUTTONS
+    // _ResetCard               DISPLAY RESET PASSWORD FIELDS AND BUTTONS
+    // _LOGINVIEW               NESTED SCROLL VIEW OF PAGE
+
+
+
     private EditText _EmailLog, _Password, _EmailReset;
     private Button _LoginBtn, _ResetBtn;
     private TextView _RegisterBtn, _ForgotPwBtn, _CancelBtn;
-    private FirebaseAuth fAuth;
     private ProgressBar progressBar;
     private CardView _LoginCard, _ResetCard;
     private NestedScrollView  _LOGINVIEW;
 
+
+    // ACTIVITY STATE MANAGEMENT METHODS
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        logHandler.printDefaultLog(LogHandler.STATE_ON_CREATE);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //BIND VIEW OBJECTS
         _LOGINVIEW = findViewById(R.id.loginVIEW);
 
         _LoginCard = findViewById(R.id.cardView);
@@ -54,45 +106,70 @@ public class LoginActivity extends AppCompatActivity {
         _EmailReset = findViewById(R.id.emailReset);
         _ResetBtn = findViewById(R.id.resetBtn);
         _CancelBtn = findViewById(R.id.cancelBtn);
-        
-        fAuth = FirebaseAuth.getInstance();
         progressBar = findViewById(R.id.progressBarLogin);
 
+        logHandler.printDefaultLog(LogHandler.VIEW_OBJECTS_BOUND);
+
+        // INITIALISE FIREBASE
+        fAuth = FirebaseAuth.getInstance();
+
+
+        //Validation for if there is a user no login required
         if(fAuth.getCurrentUser() != null){
             startActivity(new Intent(getApplicationContext(), ViewServersActivity.class));
             finish();
         }
+        logHandler.printDefaultLog(LogHandler.FIREBASE_USER_FOUND);
 
+        // SETUP VIEW OBJECTS
+        //Populate Buttons with Listeners
         _LoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Close keyboard upon button press
                 closeKeyboard();
                 String emailLog = _EmailLog.getText().toString().trim();
                 String password = _Password.getText().toString().trim();
 
+                //Validation if Email Field is empty
                 if (TextUtils.isEmpty(emailLog)) {
                     _EmailLog.setError("Email is required");
+                    logHandler.printLogWithMessage("No Email Inputted");
+
                     return;
                 }
 
+                //Validation if Password Field is empty
                 if (TextUtils.isEmpty(password)) {
                     _Password.setError("Password is required");
+                    logHandler.printLogWithMessage("No Password Inputted");
+
                     return;
                 }
 
+                //Set Progressbar to visible
                 progressBar.setVisibility(View.VISIBLE);
 
                 //authenticate User
                 fAuth.signInWithEmailAndPassword(emailLog, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                       // if successful show toast and move to ViewServerActivity
                         if (task.isSuccessful()) {
                             Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            logHandler.printLogWithMessage("Logged in Successfully");
+
+                            //NAVIGATE TO VIEW SERVER ACTIVITY
                             startActivity(new Intent(getApplicationContext(), ViewServersActivity.class));
+                            logHandler.printActivityIntentLog("View Server Activity");
+
                             finish();
 
+                        // if unsuccessful show toast and try again
                         } else {
                             Toast.makeText(LoginActivity.this, "Invalid Email or Password ", Toast.LENGTH_SHORT).show();
+                            logHandler.printLogWithMessage("Login Unsuccessful");
+
                             progressBar.setVisibility(View.INVISIBLE);
                         }
                     }
@@ -103,34 +180,53 @@ public class LoginActivity extends AppCompatActivity {
         _RegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //Navigating to Register Activity
                 startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                logHandler.printActivityIntentLog("Register Activity");
+
             }
         });
 
         _ForgotPwBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Resetting Password
+
+                //_LoginCard set to invisible, _ResetCard visible
                 _LoginCard.setVisibility(View.INVISIBLE);
+                logHandler.printLogWithMessage("Reset Password Card Opened");
             }
         });
 
         _ResetBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //TO RESET PASSWORD
+
                 String emailReset = _EmailReset.getText().toString().trim();
 
+                //DIALOG BOX AFTER SUCCESSFUL EMAIL RESET
                 final AlertDialog.Builder resetpw = new AlertDialog.Builder(v.getContext());
                 resetpw.setMessage("The Reset Link has been sent to your Email");
 
+                //PRESSING OK WILL SET _LoginCard back to visible, blocking _ResetCard
                 resetpw.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        logHandler.printLogWithMessage("User selected OK");
+
+                        //_LoginCard set to visible, _ResetCard hidden
                         _LoginCard.setVisibility(View.VISIBLE);
+
                     }
                 });
 
+                //Validation if Password Field is empty
                 if (TextUtils.isEmpty(emailReset)) {
                     _EmailReset.setError("Email is required");
+                    logHandler.printLogWithMessage("No Email Inputted");
+
                     return;
                 }
 
@@ -138,11 +234,17 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
+                            logHandler.printLogWithMessage("Reset Email has been successfully sent");
+                            //DialogBox shown
                             resetpw.create().show();
+                            logHandler.printLogWithMessage("DialogBoxShown");
+                            //Keyboard is closed
                             closeKeyboard();
 
                         } else {
                             Toast.makeText(LoginActivity.this,"Invalid Email", Toast.LENGTH_SHORT).show();
+                            logHandler.printLogWithMessage("Invalid Email Inputted");
+
                         }
 
                     }
@@ -153,10 +255,16 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+
         _CancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Cancelling reset Password function
+                logHandler.printLogWithMessage("Reset Password Cancelled");
+
+                //_LoginCard Set to Visible, hiding _ResetCard
                 _LoginCard.setVisibility(View.VISIBLE);
+
 
             }
         });
@@ -166,11 +274,15 @@ public class LoginActivity extends AppCompatActivity {
 
 
     }
+
+    //Close Keyboard
     private void closeKeyboard() {
         View view = this.getCurrentFocus();
         if (view != null) {
             InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            logHandler.printLogWithMessage("Keyboard Hidden");
+
         }
     }
 }
--- app/src/main/java/com/threadteam/thread/activities/RegisterActivity.java
+++ app/src/main/java/com/threadteam/thread/activities/RegisterActivity.java
@@ -25,34 +25,78 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.threadteam.thread.LogHandler;
 import com.threadteam.thread.R;
 import com.threadteam.thread.models.User;
 
-public class RegisterActivity extends AppCompatActivity {
-    private EditText _UserName, _Email, _Password, _CfmPassword;
-    private Button  _RegisterBtn;
-    private TextView _LoginBtn;
-    private CardView _RegisterCard;
-    private ProgressBar progressBar;
-    private NestedScrollView _REGISTERVIEW;
 
+// REGISTER ACTIVITY
+//
+// PROGRAMMER-IN-CHARGE:
+// MOHAMED THABITH, S10196396B
+//
+// DESCRIPTION
+// Handles REGISTERING OF USER
+//
+//
+// NAVIGATION
+// PARENT: NONE
+// CHILDREN: LOGIN ACTIVITY
+// OTHER: VIEW SERVER
+
+public class RegisterActivity extends AppCompatActivity {
+
+    // LOGGING
+    private LogHandler logHandler = new LogHandler("Register Activity");
+
+    // FIREBASE
+    //
+    // fAuth:                FIREBASE AUTH INSTANCE FOR THE CURRENT SESSION.
+    // reff:                 FIREBASE DATABASE REFERENCE FOR THE CURRENT SESSION.
 
     private FirebaseAuth fAuth;
     private DatabaseReference reff;
     private User user;
 
+    // VIEW OBJECTS
+    //
+    // _Username                CONTAINS USERNAME OF USER TO BE UPLOADED TO DATABASE
+    // _Email:                  CONTAINS EMAIL OF USER TO BE UPLOADED TO DATABASE
+    // _Password:               CONTAINS PASSWORD OF USER TO BE UPLOADED TO DATABASE
+    // _CfmPassword             CONTAINS CONFIRM PASSWORD OF USER TO BE CHECKED WITH _Password
+    // _RegisterBtn:            TRIGGERS REGISTERING INTO DATABASE AND LOGGING IN TO APP
+    // _LoginBtn:               TRIGGERS RETURN TO LOGIN CARD VIEW
+    // progressBar              DISPLAYS LOG IN PROGRESS ONCE _LoginBtn HAS BEEN CLICKED
+    // _REGISTERVIEW            NESTED SCROLL VIEW OF PAGE
+
+
+
+    private EditText _UserName, _Email, _Password, _CfmPassword;
+    private Button  _RegisterBtn;
+    private TextView _LoginBtn;
+    private ProgressBar progressBar;
+    private NestedScrollView _REGISTERVIEW;
+
+
+    //_aboutUsMessage           ABOUT MESSAGE IN ViewProfileActivity
+    //_statusMessage            STATUS MESSAGE IN ViewProfileActivity
+
     private String _aboutUsMessage;
     private String _statusMessage;
 
+    // ACTIVITY STATE MANAGEMENT METHODS
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        logHandler.printDefaultLog(LogHandler.STATE_ON_CREATE);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        //BIND VIEW OBJECTS
+
         _REGISTERVIEW = findViewById(R.id.registerVIEW);
 
-        _RegisterCard = findViewById(R.id.RegisterCard);
         _UserName = findViewById(R.id.userNameReg);
         _Email = findViewById(R.id.emailReg);
         _Password = findViewById(R.id.passwordReg);
@@ -66,9 +110,12 @@ public class RegisterActivity extends AppCompatActivity {
         _aboutUsMessage = "No Description";
         _statusMessage = "No Status";
 
+        // INITIALISE FIREBASE
         user = new User();
         reff = FirebaseDatabase.getInstance().getReference().child("users");
 
+        // SETUP VIEW OBJECTS
+        //Populate Buttons with Listeners
         _RegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -77,38 +124,59 @@ public class RegisterActivity extends AppCompatActivity {
                 String password =  _Password.getText().toString().trim();
                 String cfmpassword = _CfmPassword.getText().toString().trim();
 
+                //Close keyboard upon button press
                 closeKeyboard();
 
+                //Validation if Username Field is empty
                 if(TextUtils.isEmpty(username)){
                     _UserName.setError("Username is required");
+                    logHandler.printLogWithMessage("No Username Inputted");
                     return;
                 }
 
+                //Validation if Username Field has more than 2 characters
                 if(username.length() < 2 ){
                    _UserName.setError("Minimum of 3 characters is required");
+                    logHandler.printLogWithMessage("Less than 3 characters in Username");
+
                     return;
                 }
 
+                //Validation if Email Field is empty
                 if(TextUtils.isEmpty(email)){
                     _Email.setError("Email is required");
+                    logHandler.printLogWithMessage("No Email Inputted");
+
                     return;
                 }
 
+
+                //Validation if Password Field is empty
                 if(TextUtils.isEmpty(password)){
                     _Password.setError("Password is required");
+                    logHandler.printLogWithMessage("No Password Inputted");
+
                     return;
                 }
 
+                //Validation if Password Field more tha 7 characters
                 if (password.length() < 7){
                     _Password.setError("Minimum of 7 characters required");
+                    logHandler.printLogWithMessage("Less than 7 characters in Password");
+
                     return;
                 }
+
+                //Validation if Password Field and Confirm Password field match
 
                 if(!password.equals(cfmpassword)){
                     _CfmPassword.setError("Passwords do not match");
+                    logHandler.printLogWithMessage("Passwords do not match");
+
                     return;
                 }
 
+                //Set Progressbar to visible
                 progressBar.setVisibility(View.VISIBLE);
                 progressBar.getProgress();
 
@@ -116,20 +184,32 @@ public class RegisterActivity extends AppCompatActivity {
                 fAuth.createUserWithEmailAndPassword(email,cfmpassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        // if successful show toast, register User and move to ViewServerActivity
+
                         if(task.isSuccessful()){
+                            //ADD VALUES TO DATABASE
                             user.set_username(username);
                             user.set_aboutUsMessage(_aboutUsMessage);
                             user.set_statusMessage(_statusMessage);
                             String UserID  = fAuth.getCurrentUser().getUid();
-
                             reff.child(UserID).setValue(user);
+
                             Toast.makeText(RegisterActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                            logHandler.printLogWithMessage("Registered Successfully");
+
+                            //NAVIGATE TO VIEW SERVER ACTIVITY
                             startActivity(new Intent(getApplicationContext(),ViewServersActivity.class));
+                            logHandler.printActivityIntentLog("View Server Activity");
+
 
                             finish();
                         }
+
+                        // if unsuccessful show toast and try again
                         else{
                             Toast.makeText(RegisterActivity.this,task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            logHandler.printLogWithMessage("Registration Unsuccessful");
+
                             progressBar.setVisibility(View.INVISIBLE);
                         }
                     }
@@ -140,17 +220,26 @@ public class RegisterActivity extends AppCompatActivity {
         _LoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Navigating to Login Activity
+
                 startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                logHandler.printActivityIntentLog("Login Activity");
+
             }
         });
 
     }
+
+    //Close Keyboard
 
     private void closeKeyboard() {
         View view = this.getCurrentFocus();
         if (view != null) {
             InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            logHandler.printLogWithMessage("Keyboard Hidden");
+
         }
+
     }
 }
